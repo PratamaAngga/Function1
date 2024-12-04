81e8fbb23c06fec96601edc5c5cda185541b269f
PratamaAngga <anggashillnoir@gmail.com>
2024-12-04 15:34:24 +0700

    Experiment 2 before question

--- Experiment/Gratitude_20.java
+++ Experiment/Gratitude_20.java
@@ -1,7 +1,15 @@
 package Experiment;
 
 public class Gratitude_20 {
-    public static void main(String[] args) {
+    public static void sayThankyou(){
         System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like i could ask you anything.");
+    }
+    public static void sayAdditionalGreetings(String greeting) {
+        System.out.println(greeting);
+    }
+    public static void main(String[] args) {
+        sayThankyou();
+        String expression = "Thankyou... Wish you all the best!!";
+        sayAdditionalGreetings(expression);
     }
 }
