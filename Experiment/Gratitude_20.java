095fe79cd2812d74482f359d9baa723528d6f280
PratamaAngga <anggashillnoir@gmail.com>
2024-12-04 15:27:15 +0700

    Experiment 1 after question

--- Experiment/Gratitude_20.java
+++ Experiment/Gratitude_20.java
@@ -1,10 +1,7 @@
 package Experiment;
 
 public class Gratitude_20 {
-    public static void sayThankyou(){
-        System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like i could ask you anything.");
-    }
     public static void main(String[] args) {
-        sayThankyou();
+        System.out.println("Thank you for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like i could ask you anything.");
     }
 }
